e118f3ba70b941b0593a9e0ffda4e019c913ffa2
sbekberov <54951903+sbekberov@users.noreply.github.com>
2020-03-26 12:01:38 +0300

    Add files via upload

---
+++ LAB3/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+
+namespace LAB3_OOP
+{
+    class Array
+    {
+        public char[] elements;
+        public Array()
+        {
+            elements = new char[10] { 'n', 'K', 'i', 'x', 'A', 'w', 'Z', 'q', 'j', 'o' };
+        }
+
+        private int amount
+        {
+            get
+            {
+                int count = 0;
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    if ("eEyYuUiIoOaA".Contains(elements[i]))
+                        count++;
+                }
+                return count;
+            }
+        }
+
+        
+
+        public int InputArray()
+        {
+
+            Console.Write(" Elements of array: ");
+            for (int i = 0; i < elements.Length; i++)
+            {
+                Console.Write(elements[i] + ", ");
+            }
+            return 0;
+        }
+
+        public int Getamount
+        {
+            get
+            {
+                return amount;
+            }
+        }
+
+        public char this[int index]
+        {
+            get
+            {
+                
+                if (Char.IsUpper(elements[index]))
+                    return elements[index];
+                else
+                    return '*';
+            }
+            set
+            {
+                elements[index] = value;
+            }
+        }
+    }
+}
---
+++ LAB3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LAB3_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array arr = new Array();
+            arr.InputArray();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(" Amount of Vowels: " + arr.Getamount);
+            Console.WriteLine();
+            Console.WriteLine(" Result : ");
+            Console.WriteLine(arr[0] + " " + arr[1] + " " + arr[2] + " " + arr[3] + " " + arr[4] + " " + arr[5] + " " + arr[6] + " " + arr[7] + " " + arr[8] + " " + arr[9]);
+                   
+        }
+    }
+}
